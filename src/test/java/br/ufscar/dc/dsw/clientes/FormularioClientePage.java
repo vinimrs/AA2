7766ea416059bac21e4facd45fd8c730a4384e4e
Vinicius Romualdo <viniromualdo082@gmail.com>
2023-07-24 17:31:31 -0300

    listando locadoras sem login (explorar)

--- src/test/java/br/ufscar/dc/dsw/clientes/FormularioClientePage.java
+++ src/test/java/br/ufscar/dc/dsw/clientes/FormularioClientePage.java
@@ -16,14 +16,14 @@ public class FormularioClientePage extends PageObject {
 
 
     public ClientesPage cadastrarCliente(Cliente cliente) {
-        browser.findElement(By.id("cpf")).sendKeys(cliente.getCpf());
-        browser.findElement(By.id("nome")).sendKeys(cliente.getNome());
-        browser.findElement(By.id("email")).sendKeys(cliente.getEmail());
-        browser.findElement(By.id("senha")).sendKeys(cliente.getSenha());
-        browser.findElement(By.id("telefone")).sendKeys(cliente.getTelefone());
-        browser.findElement(By.id("sexo")).sendKeys(cliente.getSexo());
-        browser.findElement(By.id("dataNascimento"))
-                .sendKeys(cliente.getDataNascimento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+//        browser.findElement(By.id("cpf")).sendKeys(cliente.getCpf());
+//        browser.findElement(By.id("nome")).sendKeys(cliente.getNome());
+//        browser.findElement(By.id("email")).sendKeys(cliente.getEmail());
+//        browser.findElement(By.id("senha")).sendKeys(cliente.getSenha());
+//        browser.findElement(By.id("telefone")).sendKeys(cliente.getTelefone());
+//        browser.findElement(By.id("sexo")).sendKeys(cliente.getSexo());
+//        browser.findElement(By.id("dataNascimento"))
+//                .sendKeys(cliente.getDataNascimento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
 
         browser.findElement(By.id("submit-button")).click();
 
